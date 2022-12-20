ab1e5499836330264739d3bfa6c39305443009e7
Angelus McNally <amcnally@microsoft.com>
2022-12-20 13:31:40 -0800

    add questions, send once a week

--- bot/src/timerTrigger.js
+++ bot/src/timerTrigger.js
@@ -2,16 +2,40 @@ const notificationTemplate = require("./adaptiveCards/notification-default.json"
 const { AdaptiveCards } = require("@microsoft/adaptivecards-tools");
 const { bot } = require("./internal/initialize");
 
+let questionList = "";
+let question = "";
+const fs = require('fs');
+const filename = "questions.txt";
+
+function returnToFile(questions) {
+  let returnedList = "";
+  for(item in questions) {
+    returnedList = returnedList.concat(questions[item] + "\r\n");
+  }
+  return returnedList;
+}
+
 // Time trigger to send notification. You can change the schedule in ../timerNotifyTrigger/function.json
 module.exports = async function (context, myTimer) {
-  const timeStamp = new Date().toISOString();
-  for (const target of await bot.notification.installations()) {
+  const timeStamp = new Date().toLocaleDateString();
+  for (const target of await bot.notification. installations()) {
+
+    fs.readFile(filename, (err, data) => {
+      if (err) throw err;
+        questionList = data.toString().split("\r\n");
+        question = questionList[0];
+        let linesExceptFirst = questionList.slice(1);
+        fs.writeFile(filename, returnToFile(linesExceptFirst), (err) => {
+          if (err) throw err;
+        });
+    })
+
     await target.sendAdaptiveCard(
       AdaptiveCards.declare(notificationTemplate).render({
-        title: "New question of the week",
+        title: `Week of ${timeStamp}`,
         appName: "Ice Fishing",
-        description: `This is a time-triggered notification (${timeStamp}).`,
-        notificationUrl: "https://www.adaptivecards.io/",
+        description: `Here's this week's question: ${question}`,
+        notificationUrl: "https://forms.microsoft.com/Pages/ResponsePage.aspx?id=v4j5cvGGr0GRqy180BHbRx4g9KFDxqlNulIX5d2_cUFUNTJUM0tWU1ZIQjBEN0RNSE4zSUhBMElOVS4u",
       })
     );
   }
